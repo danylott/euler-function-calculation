1739f91a5854f711826ade9d0b2705101e4eda4c
danylott <d.tuytuyshkin@gmail.com>
2022-04-10 15:04:31 +0300

    added more tests

--- test_main.py
+++ test_main.py
@@ -18,5 +18,30 @@ from main import euler_function_calculation
         (10, 4)
     ]
 )
-def test_euler_function_calculation(value, result):
+def test_euler_function_calculation_simple(value, result):
     assert euler_function_calculation(value) == result
+
+
+@pytest.mark.parametrize(
+    "value,result",
+    [
+        (13, 12),
+        (17, 16),
+        (131, 130),
+    ]
+)
+def test_euler_function_calculation_prime(value, result):
+    assert euler_function_calculation(value) == result
+
+
+@pytest.mark.parametrize(
+    "first,second,third",
+    [
+        (165, 11, 16),
+        (12, 3, 4),
+        (221, 13, 17),
+        (494, 19, 26),
+    ]
+)
+def test_euler_function_calculation_multiplication(first, second, third):
+    assert euler_function_calculation(first) == euler_function_calculation(second) * euler_function_calculation(third)
